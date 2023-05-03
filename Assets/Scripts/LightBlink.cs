5ef26d2b0c398d67a5e1ba5480e4ad84aca4c1f0
bpinedad <bpinedad96@gmail.com>
2023-05-03 00:18:47 -0400

    Added user lights

--- Assets/Scripts/LightBlink.cs
+++ Assets/Scripts/LightBlink.cs
@@ -4,48 +4,26 @@ using UnityEngine;
 
 public class LightBlink : MonoBehaviour
 {
-    Light myLight;
+    [SerializeField] float freq = 1f;
+    [SerializeField] float minIntensity = 1f;
+    [SerializeField] float maxIntensity = 10f;
 
-    bool blinking = false;
-    [SerializeField] float blinkSpeed = 0.1f;
-    [SerializeField] float minIntensity = 5f;
-    [SerializeField] float maxIntensity = 5f;
-    float lastTime = 0f;
+    Light myLight;
 
     // Start is called before the first frame update
     void Awake()
     {
-        myLight = GetComponent<Light>();
+        //Get light component
+        myLight = GetComponent<Light>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > lastTime + 5)
-        {
-            //Test pingpong
-            myLight.enabled = !myLight.enabled;
-            lastTime = Time.time;
-        }
+        //Calculate sin function
+        float sinFunc = Mathf.Sin(freq * Time.time) * 0.5f + 0.5f;
 
-/*
-        //If turning on light
-        if (blinking) {
-            //Increase each frame by speed
-            myLight.intensity += blinkSpeed;
-            //If at max intensity revert
-            if (myLight.intensity > maxIntensity) {
-                blinking = false;
-            }
-            //Opposite
-        } else {
-            GmyLight.intensity -= blinkSpeed;
-            if (myLight.intensity < minIntensity) {
-                blinking = true;
-            }
-        }
-
-*/
-        
+        //Adjust light
+        myLight.intensity = minIntensity + maxIntensity * sinFunc;
     }
 }
